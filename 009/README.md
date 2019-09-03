862f3acce814e192a2a87df8d1ea5893564025af
francis <yinghao_liu@163.com>
2019-09-03 16:31:32 +0800

    echo server using libuv

--- 009/README.md
+++ 009/README.md
@@ -8,3 +8,4 @@ log system which is interactable
 ## reference
 1. [event-loop](http://www.ruanyifeng.com/blog/2014/10/event-loop.html)
 2. [libuv](http://luohaha.github.io/Chinese-uvbook/source/introduction.html)
+3. [libuv doc](http://docs.libuv.org/en/v1.x/index.html)
--- 009/main.cpp
+++ 009/main.cpp
@@ -15,6 +15,7 @@ Welcome to interactable log system
 
 uv_loop_t *g_loop;
 uv_buf_t write_buf;
+
 void on_write(uv_write_t* req, int status)
 {
 	printf("on_write:%d\n", status);	
@@ -23,20 +24,22 @@ void on_write(uv_write_t* req, int status)
 
 void process_request(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf)
 {
-	printf("echo read, nread: %zd, buf->len:%zu\n", nread, buf->len);
+	printf("echo read, nread: %zd, buf->len:%zx\n", nread, buf->len);
 	if (0 != nread && nullptr != buf->base)
 	printf("[%s]\n", buf->base);
 	uv_write_t *req = (uv_write_t *)malloc(sizeof (uv_write_t));
-	write_buf = uv_buf_init((char *)malloc(nread), nread-1);
-	strncpy(write_buf.base, "aa login:", write_buf.len);
+	write_buf = uv_buf_init((char *)malloc(nread), nread);
+	strncpy(write_buf.base, buf->base, write_buf.len);
 	uv_write(req, stream, &write_buf, 1, on_write);
 	free(buf->base);
 }
 
 void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf)
 {
+	printf("suggested_size:%zu\n", suggested_size);
 	*buf = uv_buf_init((char *)malloc(suggested_size), suggested_size);
 	if (nullptr != buf->base) {
+		printf("buf->base not null\n");
 		memset(buf->base, 0, buf->len);
 	}
 	printf("alloc_buffer:%zd\n", suggested_size);
@@ -48,11 +51,16 @@ void on_new_connection(uv_stream_t* server, int status)
 	uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
 	uv_tcp_init(g_loop, client);
 	if (uv_accept(server, (uv_stream_t*) client) == 0) {
-		write_buf = uv_buf_init((char *)malloc(10), 9);
-		strncpy(write_buf.base, "aa login:", 9);
+		size_t len = strlen(home_page);
+		write_buf = uv_buf_init((char *)malloc(len), len);
+		strncpy(write_buf.base, home_page, write_buf.len);
 		uv_write_t *req = (uv_write_t *)malloc(sizeof (uv_write_t));
-		int ret = uv_write(req, (uv_stream_t*)client, &write_buf, 1, on_write);
-		printf("%s\n", uv_strerror(ret));
+		int ret;
+		// uv_write write write_buf.len bytes
+		ret = uv_write(req, (uv_stream_t*)client, &write_buf, 1, on_write);
+		if (-1 == ret) {
+			printf("%s\n", uv_strerror(ret));
+		}
 		uv_read_start((uv_stream_t*) client, alloc_buffer, process_request);
 	}
 }
@@ -65,7 +73,7 @@ int main(void )
 	uv_tcp_init(g_loop, &server);
 	
 	int ret;
-	 struct sockaddr_in addr;
+	struct sockaddr_in addr;
 	ret = uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
 
 	ret = uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
